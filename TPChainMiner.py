49ffdcb1de57067c7003d94ba3d404bb1c8aae75
cpchia <chawchia@hotmail.co.uk>
2020-12-11 01:39:34 +0800

    Post back to node mined block

--- TPChainMiner.py
+++ TPChainMiner.py
@@ -1,7 +1,8 @@
 from Mining import Mining
 import requests
 
-query = {'Miner':"123.123.123.123"}
+MinerIP="123.123.123.123"
+query = {'Miner':MinerIP}
 response = requests.get('http://127.0.0.1:1234/mining/get-mining-job', params=query)
 miningDict=response.json()
 
@@ -10,5 +11,8 @@ mining.mine()
 blockHashIsVerified=mining.verifyBlockHash()
 if (blockHashIsVerified):
     print("Block is verified! Block was mined successfull with nonce: " + str(mining.nonce))
+    minedBlock={'MinedBy':MinerIP,"Blockindex":miningDict["Index"], "MinedNonce":str(mining.nonce)}
+    BlockSubmissionResponse = requests.post('http://127.0.0.1:1234/mining/submit-mined-block', minedBlock)
 else:
     print("Block nonce: " + str(mining.nonce) + " is INCORRECT for this block, please try to mine again")
+
